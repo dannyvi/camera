59890ab9b26e9f0f7cfaa96cff42053e0f02e83d
dandany <yu.yuyudan@gmail.com>
2016-03-11 17:32:58 +0800

    Initial Commit

---
+++ CameraM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraM
+//
+//  Created by DannyV on 16/3/11.
+//  Copyright © 2016年 YuDan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
